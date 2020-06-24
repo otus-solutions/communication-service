4248ae72b6f7b36b4e4db118cd9e86c628bad192
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-24 18:17:49 -0300

    CS-4 troca da variavel local configReady em ElasticsearchService pela var de ambiente CONFIG_READY

--- source/app/services/ElasticsearchService.js
+++ source/app/services/ElasticsearchService.js
@@ -1,4 +1,5 @@
 const {Client} = require('@elastic/elasticsearch');
+require('custom-env').env('staging');
 
 const {
     ELASTICSEARCH_PORT,
@@ -6,28 +7,31 @@ const {
 } = process.env;
 
 
-/** @namespace application.app.services.ElasticsearchService **/
-
-let configReady = true;
-
 //todo: test if env var are present
 
-module.exports = function (application) {
+/** @namespace application.app.services.ElasticsearchService **/
+module.exports = {
 
-    return {
-         getClient() {
-            if(!configReady){
-                throw new Error("ElasticService.getClient initialization error");
-            }
-            return new Client({node: ELASTICSEARCH_HOSTNAME + ":" + ELASTICSEARCH_PORT});
-        },
+    getClient: function() {
+        let configReady = (process.env.CONFIG_READY === 'true');
+        console.log('\nElasticService.getClient: configReady =', configReady)
 
-        setState(state) {
-            console.log('\nElasticService.setState before: configReady =', configReady)
-            console.log('ElasticService.setState', state)
-            configReady = !!state;
-            console.log('ElasticService.setState after : configReady =', configReady)
+        if(!configReady){
+            throw new Error("ElasticService.getClient initialization error");
         }
+        return new Client({node: ELASTICSEARCH_HOSTNAME + ":" + ELASTICSEARCH_PORT});
+    },
+
+    setState: function(state) {
+        console.log('\nElasticService.setState before: configReady =', process.env.CONFIG_READY)
+        console.log('ElasticService.setState', state)
+        process.env.CONFIG_READY = (!!state).toString();
+        console.log('ElasticService.setState after : configReady =', process.env.CONFIG_READY)
+    },
+
+    print: function () {//.
+        let configReady = (process.env.CONFIG_READY === 'true');
+        console.log('configReady =', configReady);
     }
 
 };
