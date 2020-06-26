97203ebf23f537575352458cfc147b11776e0cde
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-26 16:37:55 -0300

    CS-4 arquivo de dados úteis para os testes

---
+++ source/test/data/json-importer.js
@@ -0,0 +1,34 @@
+module.exports = {
+    issue: {
+        objectType: "Issue",
+        sender: "5",
+        group: "3",
+        title: "Não consigo preencher a atividade TCLEC",
+        text: "Quando tento responder uma pergunta, não consigo inserir a resposta",
+        creationDate: "2020-04-20T15:30:07.533Z",
+        status: "OPEN"
+    },
+    issueHit: {
+        _index: 'issues',
+        _type: '_doc',
+        _id: 'fqX38XIBU9yBIlNWf2l5',
+        _score: null,
+        _source: {
+            sender: '4',
+            group: '3',
+            status: 'OPEN',
+            title: 'Não consigo preencher a atividade TCLEC',
+            text: 'Quando tento responder uma pergunta, não consigo inserir a resposta',
+            creationDate: '2020-06-20T15:30:07.533Z'
+        },
+        sort: [ 1592667007533, '3', '4' ]
+    },
+    message: {
+        "text": "conexão ok, qual próximo passo",
+        "sender": "fulano3@email.com",
+        "creationDate": "2020-04-12T11:30:07.533Z",
+        "issueId": "fqX38XIBU9yBIlNWf2l5"
+    },
+    messageHit: {}
+
+};
