dc813045206b26295e6669ec260ef3bd0f82538e
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-29 10:07:55 -0300

    CS-4 add testes unitários de ElasticsearchService
    'getClient_in_case_client_already_exists',  'getState'

--- source/test/services/ElasticsearchService-spec.js
+++ source/test/services/ElasticsearchService-spec.js
@@ -16,9 +16,19 @@ describe('ElasticsearchService_Suite_Tests', function () {
         expect(() => ElasticsearchService.getClient()).toThrow();
     });
 
+    test('getClient_in_case_client_already_exists', async function () {
+        const client1 = ElasticsearchService.getClient();
+        const client2 = ElasticsearchService.getClient();
+        expect(client2).toBe(client1);
+    });
+
     test('getIndices', async function () {
         const indices = ElasticsearchService.getIndicesAPI();
         expect(indices).toBeDefined();
+    });
+
+    test('getState', async function () {
+        expect(ElasticsearchService.getState()).toBe(true);
     });
 
 });
