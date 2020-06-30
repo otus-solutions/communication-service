88d05b0adbef10200645f82ec065c316a0d2f6a9
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-30 03:22:55 -0300

    CS-4 add data.messageHit, data.client.deleteByQuery em json-importer.js

--- source/test/data/json-importer.js
+++ source/test/data/json-importer.js
@@ -11,16 +11,16 @@ data.issue = {
 data.issueId = 'fqX38XIBU9yBIlNWf2l5';
 data.issueHit = {
     _index: 'issues',
-        _type: '_doc',
-        _id: data.issueId,
-        _score: null,
-        _source: {
-            sender: "5",
-            group: "3",
-            title: "Não consigo preencher a atividade TCLEC",
-            text: "Quando tento responder uma pergunta, não consigo inserir a resposta",
-            creationDate: "2020-04-20T15:30:07.533Z",
-            status: "OPEN"
+    _type: '_doc',
+    _id: data.issueId,
+    _score: null,
+    _source: {
+        sender: "5",
+        group: "3",
+        title: "Não consigo preencher a atividade TCLEC",
+        text: "Quando tento responder uma pergunta, não consigo inserir a resposta",
+        creationDate: "2020-04-20T15:30:07.533Z",
+        status: "OPEN"
     },
     sort: [ 1592667007533, '3', '4' ]
 };
@@ -42,12 +42,25 @@ data.issuesSearchSettings = {
 };
 
 data.message = {
-    "text": "conexão ok, qual próximo passo",
-        "sender": "fulano3@email.com",
-        "creationDate": "2020-04-12T11:30:07.533Z",
-        "issueId": "fqX38XIBU9yBIlNWf2l5"
+    text: "conexão ok, qual próximo passo",
+    sender: "fulano3@email.com",
+    creationDate: "2020-04-12T11:30:07.533Z",
+    issueId: data.issueId
 };
-data.messageHit = {};
+data.messageId = 'ylSfA3MBWwgW4b7GqwXi';
+data.messageHit = {
+    _index: "messages",
+    _type: '_doc',
+    _id: data.messageId,
+    _score: null,
+    _source: {
+        text: "conexão ok, qual próximo passo",
+        sender: "fulano3@email.com",
+        creationDate: "2020-04-12T11:30:07.533Z",
+        issueId: data.issueId
+    },
+    sort: [ 1592667007533, '3', '4' ]
+};
 
 data.client = {
     index: function(obj) {return {}},
@@ -55,7 +68,8 @@ data.client = {
     get: function(issueId) {return {}},
     update: function(obj) {return {}},
     exists: function(obj) {return {}},
-    delete: function(obj) {return {}}
+    delete: function(obj) {return {}},
+    deleteByQuery: function(obj) {return {}},
 };
 
 
